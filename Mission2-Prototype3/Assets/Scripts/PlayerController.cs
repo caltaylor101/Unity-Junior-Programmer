54edae63b5017be7a95ee4c4a6867f6b6df21157
Cody Llamas <34333250+caltaylor101@users.noreply.github.com>
2023-07-20 19:16:56 -0600

    Lesson 3.4 Completed

--- Mission2-Prototype3/Assets/Scripts/PlayerController.cs
+++ Mission2-Prototype3/Assets/Scripts/PlayerController.cs
@@ -10,12 +10,19 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
-    private Animator playerAnim; 
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio; 
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
-        playerAnim = GetComponent<Animator>(); 
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>(); 
     }
 
     private void Update()
@@ -25,6 +32,8 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop(); 
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
@@ -32,14 +41,18 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            isOnGround = true; 
+            isOnGround = true;
+            dirtParticle.Play(); 
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
-            playerAnim.SetInteger("DeathType_int", 1); 
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop(); 
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 
